7b892e68872d7b8db1c6045c6c830a4a24dc0256
Absolutely <absolutely1756@gmail.com>
2020-03-30 23:31:00 +0700

    Added test game board

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square"
+                    onClick={() => this.props.onClick( )}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(361).fill(null)
+        };
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i]='X';
+        this.setState({squares:squares});
+    }
+    renderSquare(i) {
+        return (<Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+        );
+    }
+    render() {
+        const table = [];
+        for (var i=0;i<19;i++) {
+            const children = [];
+            for (var k = 0; k < 19; k++)
+                children.push(this.renderSquare(19*i+k));
+            table.push(<div className={'board-row'}>{children}</div>);
+        }
+        return (
+            <div>
+                {table}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
